796bb9e682c4c4fa91bf5d867d1e46bda207369b
ReardonMetal27 <timlosser+rm27@gmail.com>
2021-05-18 03:15:58 -0700

    Cleaned up spacing.

--- InputCommands/AV8BNA/Input/AV8BNA/mouse/default.lua
+++ InputCommands/AV8BNA/Input/AV8BNA/mouse/default.lua
@@ -1,6 +1,6 @@
 return {
 	keyCommands = {
 	-- Enable binding the flashlight to Mouse buttons
-		{down = 3256,	cockpit_device_id = 0,	value_down = 1.0,						name = _('Flashlight'),						category = _('View Cockpit')}
+		{down = 3256,	cockpit_device_id = 0,	value_down = 1.0, name = _('Flashlight'), category = _('View Cockpit')}
 	}
 }
