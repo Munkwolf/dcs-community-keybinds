180f805c5c55d5d331830b86301403dce95bd7f0
ReardonMetal27 <timlosser+rm27@gmail.com>
2021-05-18 03:03:27 -0700

    F-16C - Autopilot Pitch Mode, Air Refueling Door FA-18C - Landing Gear AV8BNA - Enable "flashlight" bound to mouse

--- InputCommands/FA-18C/Input/FA-18C/joystick/default.lua
+++ InputCommands/FA-18C/Input/FA-18C/joystick/default.lua
@@ -1,5 +1,9 @@
 return {
 	keyCommands = {
 		{down = 3107, up = 3107, cockpit_device_id = 0, value_down = 1.0, value_up = 0.0, name = _('HUD Video BIT Initiate Pushbutton'), category = {_('Instrument Panel'), _('UFC')}}, 
+	-- Landing Gear - Special for Joystick
+		{down = iCommandPlaneGearDown, up = iCommandPlaneGearUp, name = _('Landing Gear Control Handle - DOWN else UP'), category = {_('Special For Joystick'), _('Left Console'), _('Custom Binds')}},
+		{down = iCommandPlaneGearUp, up = iCommandPlaneGearDown, name = _('Landing Gear Control Handle - Up else Down'), category = {_('Special For Joystick'), _('Left Console'), _('Custom Binds')}},
+
 	}
 }
