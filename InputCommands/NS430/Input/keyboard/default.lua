dbaafd29f569f681b94965770c1b4b5ca9e137ba
David Pierron <dpierron@gmail.com>
2021-06-26 14:07:53 +0200

    added NS430 "button down" commands

--- InputCommands/NS430/Input/keyboard/default.lua
+++ InputCommands/NS430/Input/keyboard/default.lua
@@ -1,5 +1,13 @@
 return {
 	keyCommands = {
 		
+		-- iCommand_left_big_axis
+		{down = device_commands.Button_8, cockpit_device_id = device_id, value_down = 0.5, name = _('Left big knob Increase DOWN'), category = _('NS Device buttons down')},
+		{down = device_commands.Button_8, cockpit_device_id = device_id, value_down = -0.5, name = _('Left big knob Decrease DOWN'), category = _('NS Device buttons down')},
+
+		-- iCommand_left_small_axis
+		{down = device_commands.Button_11, cockpit_device_id = device_id, value_down = 0.5, name = _('Left small knob Increase DOWN'), category = _('NS Device buttons down')},
+		{down = device_commands.Button_11, cockpit_device_id = device_id, value_down = -0.5, name = _('Left small knob Decrease DOWN'), category = _('NS Device buttons down')},
+
 	}
 }
