42e1bb735538d0b128896c41542349c8fde0131f
David Pierron <dpierron@gmail.com>
2021-06-26 14:08:27 +0200

    added FLAPS and IDLE/OFF 3-way commands for A10 and A10-2

--- InputCommands/A-10C/Input/A-10C/joystick/default.lua
+++ InputCommands/A-10C/Input/A-10C/joystick/default.lua
@@ -1,5 +1,11 @@
 return {
 	keyCommands = {
+
+		-- Added specific 3-way switch commands for FLAPS and Throttle OFF/IDLE
+		{down = iCommandLeftEngineStop , up = iCommandLeftEngineStart,  name = _('Left Throttle OFF/IDLE') , category = _('Special')},
+		{down = iCommandRightEngineStop, up = iCommandRightEngineStart, name = _('Right Throttle OFF/IDLE'), category = _('Special')},
+		{down = iCommandPlane_FLAPS_UP, up = iCommandPlane_FLAPS_MNR_from_UP,name = _('Flaps Up/MR'), category = _('Special')},
+		{down = iCommandPlane_FLAPS_DN, up = iCommandPlane_FLAPS_MNR_from_DN,name = _('Flaps MR/Dn'), category = _('Special')},
 		
 	}
 }
