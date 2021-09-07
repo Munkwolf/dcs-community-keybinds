7cca5882530e8728b634579bb5589e9911019335
Munkwolf <dmmccarley+dcs@gmail.com>
2021-09-07 07:47:56 -0600

    P-47D: Adding a few binds, air filter and hydraulic hand pump axis, and a fix for stock radio lock lever.

--- InputCommands/P-47D-30/Input/P-47D-30/joystick/default.lua
+++ InputCommands/P-47D-30/Input/P-47D-30/joystick/default.lua
@@ -1,5 +1,24 @@
 return {
 	keyCommands = {
-		
+
+		-- Radio Remote Channel Switcher
+
+		{down = device_commands.Button_17, cockpit_device_id = devices.VHF_RADIO, value_down = 1.0, name = _('Radio Mode Lock Switch On'), category = {_('VHF Radio'), _('Custom')}},
+		{down = device_commands.Button_17, cockpit_device_id = devices.VHF_RADIO, value_down = 0.0, name = _('Radio Mode Lock Switch Off'), category = {_('VHF Radio'), _('Custom')}},
+		{down = device_commands.Button_17, up = device_commands.Button_17, cockpit_device_id = devices.VHF_RADIO, value_down = 1.0, value_up = 1.0, name = _('Radio Mode Lock Switch On else Off (2-way Switch)'), category = {_('Systems'), _('Custom')}},
+
+		-- Oxygen Apparatus
+
+		{down = device_commands.Button_5, cockpit_device_id = devices.OXYGEN, value_down = 1.0, name = _('Oxygen Emergency By-pass On'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_5, cockpit_device_id = devices.OXYGEN, value_down = 0.0, name = _('Oxygen Emergency By-pass Off'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_5, up = device_commands.Button_5, cockpit_device_id = devices.OXYGEN, value_down = 1.0, value_up = 1.0, name = _('Oxygen Emergency By-pass On else Off (2-way Switch)'), category = {_('Systems'), _('Custom')}},
+
+		{down = device_commands.Button_3, cockpit_device_id = devices.OXYGEN, value_down = 1.0, name = _('Auto-Mix On'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_3, cockpit_device_id = devices.OXYGEN, value_down = 0.0, name = _('Auto-Mix Off'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_3, up = device_commands.Button_3, cockpit_device_id = devices.OXYGEN, value_down = 1.0, value_up = 1.0, name = _('Auto-Mix On else Off (2-way Switch)'), category = {_('Systems'), _('Custom')}},
+	},
+	axisCommands = {
+		{action = device_commands.Button_33, cockpit_device_id = devices.ENGINE_CONTROLS, name = _('Air Filter Lever')},
+		{action = device_commands.Button_52, cockpit_device_id = devices.CONTROLS, name = _('Emergency Hydraulic Hand Pump')},
 	}
 }
--- InputCommands/P-47D-30/Input/P-47D-30/keyboard/default.lua
+++ InputCommands/P-47D-30/Input/P-47D-30/keyboard/default.lua
@@ -1,5 +1,20 @@
 return {
 	keyCommands = {
-		
+
+		-- Radio Remote Channel Switcher
+
+		{down = device_commands.Button_17, cockpit_device_id = devices.VHF_RADIO, value_down = 1.0, name = _('Radio Mode Lock Switch On'), category = {_('VHF Radio'), _('Custom')}},
+		{down = device_commands.Button_17, cockpit_device_id = devices.VHF_RADIO, value_down = 0.0, name = _('Radio Mode Lock Switch Off'), category = {_('VHF Radio'), _('Custom')}},
+		{down = device_commands.Button_17, up = device_commands.Button_17, cockpit_device_id = devices.VHF_RADIO, value_down = 1.0, value_up = 1.0, name = _('Radio Mode Lock Switch On else Off (2-way Switch)'), category = {_('Systems'), _('Custom')}},
+
+		-- Oxygen Apparatus
+
+		{down = device_commands.Button_5, cockpit_device_id = devices.OXYGEN, value_down = 1.0, name = _('Oxygen Emergency By-pass On'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_5, cockpit_device_id = devices.OXYGEN, value_down = 0.0, name = _('Oxygen Emergency By-pass Off'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_5, up = device_commands.Button_5, cockpit_device_id = devices.OXYGEN, value_down = 1.0, value_up = 1.0, name = _('Oxygen Emergency By-pass On else Off (2-way Switch)'), category = {_('Systems'), _('Custom')}},
+
+		{down = device_commands.Button_3, cockpit_device_id = devices.OXYGEN, value_down = 1.0, name = _('Auto-Mix On'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_3, cockpit_device_id = devices.OXYGEN, value_down = 0.0, name = _('Auto-Mix Off'), category = {_('Systems'), _('Custom')}},
+		{down = device_commands.Button_3, up = device_commands.Button_3, cockpit_device_id = devices.OXYGEN, value_down = 1.0, value_up = 1.0, name = _('Auto-Mix On else Off (2-way Switch)'), category = {_('Systems'), _('Custom')}},
 	}
 }
