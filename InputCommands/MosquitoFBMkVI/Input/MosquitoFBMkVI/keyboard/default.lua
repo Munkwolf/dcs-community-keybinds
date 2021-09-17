614d5e39277abe94a185bbbf6768f4825cac5b08
Munkwolf <dmmccarley+dcs@gmail.com>
2021-09-17 16:25:14 -0600

    Mosquito: Initial add of files.

---
+++ InputCommands/MosquitoFBMkVI/Input/MosquitoFBMkVI/keyboard/default.lua
@@ -0,0 +1,6 @@
+return {
+	keyCommands = {
+		{cockpit_device_id = devices.SWITCHBOARD, down = device_commands.Button_18, up = device_commands.Button_18, value_down = 1, value_up = 0, name = _('Landing Light, port side On else Off (2-way Switch)'), category = {_('Front Dash'), _('Exterior Lights'), _('Electrical System'), _('Custom')}},	
+		{cockpit_device_id = devices.SWITCHBOARD, down = device_commands.Button_20, up = device_commands.Button_20, value_down = 1, value_up = 0, name = _('Landing Light, port side On else Off (2-way Switch)'), category = {_('Front Dash'), _('Exterior Lights'), _('Electrical System'), _('Custom')}},	
+	}
+}
